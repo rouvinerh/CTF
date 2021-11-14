655efa09b9191a8238bb9fcc503c978ce6d56b3e
rouvinerh <91769556+rouvinerh@users.noreply.github.com>
2021-11-14 15:21:01 +0800

    Update gandalf.py

--- CTFlearn/Easy/GandalfTheWise/gandalf.py
+++ CTFlearn/Easy/GandalfTheWise/gandalf.py
@@ -14,5 +14,14 @@ i=0
 while i < length:
     flag.append(chr(string1[i] ^ string2[i]))
     i+=1
-
+flag = ''.join(flag)
 print (flag)
+
+# for reference:
+# XOR works by comparing the bits of the information in each string and changing it based on whether it is 
+#the same or different
+# this program creates an empty list to add each character of the flag together
+# ^ is the XOR function otherwise knwon as exclusive  or and compares two binary NUMBERS bitwise.
+# It can only accept a number, and chr converts each character back to it's ASCII value to generate a long string.
+# this would result in the code being able to compare each ASCII character to XOR them to generate one character appended to the list.
+# Resulting solution is the flag after joining the elements together.
