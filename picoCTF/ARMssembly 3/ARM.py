ebff2ef6437d3432c2f2d824f9b61c18dfe12859
rouvinerh <91769556+rouvinerh@users.noreply.github.com>
2021-12-02 22:37:02 +0800

    Create ARM.py

---
+++ picoCTF/ARMssembly 3/ARM.py
@@ -0,0 +1,12 @@
+input = 469937816
+def func1 (x):
+    count = 0
+    while x != 0:
+        if x & 1 != 0:
+            count +=3
+        x >> 1
+
+    return count
+
+ans = func1 (input)
+print (ans)
